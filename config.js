be23e7e51cad09d28e7ee8112326d79ae6383d41
vihangakavishka <vihagakavishka6@gmail.com>
2026-07-13 14:46:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/vihangakavishka/wiki-/blob/main/imges/20260713_130803.jpg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 WIKI AI  Is Help Now😍*",
+BOT_OWNER: '94766078596',  // Replace with the owner's phone number
 
 
 
